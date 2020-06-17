ff3b4d075827fa3f69f63100273a322129ebbdff
skim <skim@swainslane.co>
2020-06-17 08:39:19 -0700

    adds exercise

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -23,36 +23,22 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+#[macro_export]
+macro_rules! planet {
+    ($x:ident, $y:expr) => {
+        pub struct $x;
 
-impl Planet for Mercury {
-    const ORBITAL_PERIOD: f64 = 0.2408467;
+        impl Planet for $x {
+            const ORBITAL_PERIOD: f64 = $y;
+        }
+    };
 }
-impl Planet for Venus {
-    const ORBITAL_PERIOD: f64 = 0.61519726;
-}
-impl Planet for Earth {
-    const ORBITAL_PERIOD: f64 = 1.0;
-}
-impl Planet for Mars {
-    const ORBITAL_PERIOD: f64 = 1.8808158;
-}
-impl Planet for Jupiter {
-    const ORBITAL_PERIOD: f64 = 11.862615;
-}
-impl Planet for Saturn {
-    const ORBITAL_PERIOD: f64 = 29.447498;
-}
-impl Planet for Uranus {
-    const ORBITAL_PERIOD: f64 = 84.016846;
-}
-impl Planet for Neptune {
-    const ORBITAL_PERIOD: f64 = 164.79132;
-}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
